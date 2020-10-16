e219dc3c4390ae142d7c681feab256a08370907c
bedrantirak <bdrntrk@gmail.com>
2020-10-16 19:43:57 +0300

    Update App.js

--- react-navbar-2/src/App.js
+++ react-navbar-2/src/App.js
@@ -1,12 +1,28 @@
 import React from 'react';
-import {BrowserRouter as Router } from 'react-router-dom';
+import {BrowserRouter as Router,Switch,Route } from 'react-router-dom';
 import './App.css';
 import Navbar from "./components/Navbar"
-
+import Consulting from './components/pages/Consulting';
+import ContactUs from './components/pages/ContactUs';
+import Marketing from './components/pages/Marketing';
+import Products from './components/pages/Products';
+import Services from './components/pages/Services';
+import SignUp from './components/pages/SignUp';
+import Home from "./components/pages/Home";
 function App() {
   return (
     <Router>
       <Navbar></Navbar>
+      <Switch>
+        <Route path="/" exact component={Home}></Route>
+        <Route path="/services" exact component={Services}></Route>
+        <Route path="/products" exact component={Products}></Route>
+        <Route path="/contact-us" exact component={ContactUs}></Route>
+        <Route path="/sign-up" exact component={SignUp}></Route>
+        <Route path="/marketing" exact component={Marketing}></Route>
+        <Route path="/consulting" exact component={Consulting}></Route>
+        
+      </Switch>
       </Router>
   );
 }
